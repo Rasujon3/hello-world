57cd6d9a558fdc51b474ab970f4e8ecb8c2ffad1
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-10-12 01:58:41 +0600

    rendering route
    14.4

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import BookList from './lists/BookList';
 import bookList from '../assets/books';
 import NewBook from './representational/NewBook';
+import { Route } from 'react-router';
 
 class MainComponent extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            books: bookList,
-            showBooks: true
+            books: bookList
+            // showBooks: true
         }
-        console.log("MainComponent Constructor");
     }
     
 
@@ -35,44 +35,46 @@ class MainComponent extends Component {
         });
     };
 
-    toggleBooks = () => {
-        this.setState({ showBooks: !this.state.showBooks });
-    }
+    // toggleBooks = () => {
+    //     this.setState({ showBooks: !this.state.showBooks });
+    // }
 
     render() {
-        const style = {
-            border: "1px solidd red",
-            borderRadius: "5px",
-            backgroundColor: "black",
-            color: "white"
-        };
+        // const style = {
+        //     border: "1px solidd red",
+        //     borderRadius: "5px",
+        //     backgroundColor: "black",
+        //     color: "white"
+        // };
 
         // const bookState = this.state.books;
-        let books = null;
-        if (this.state.showBooks) {
-            books = <BookList books={this.state.books}
+        // let books = null;
+        // if (this.state.showBooks) {
+          const  books = <BookList books={this.state.books}
                 deleteBookState={this.deleteBookState}
                 changeWithInputState={this.changeWithInputState}
 
             />
-        }
+        // }
 
 
         // console.log(bookState);
-        console.log(books);
+        // console.log(books);
 
         return (
             <div className="App">
                 <div className="nav-bar">
                     <ul>
                         <li><a href="/">Home</a></li>
-                        <li><a href="/new">New Book</a></li>
+                        <li><a href="/new-book">New Book</a></li>
                     </ul>
                 </div>
-                <h1 style={style}>Book List</h1>
-                <button onClick={this.toggleBooks}>Toggle Books</button>
-                {books}
-                <NewBook/>
+                {/* <h1 style={style}>Book List</h1> */}
+                {/* <button onClick={this.toggleBooks}>Toggle Books</button> */}
+                {/* {books}
+                <NewBook/> */}
+                <Route path="/" exact render={()=> <h1>Home</h1> } />
+                <Route path="/new-book" exact render={()=> <h1>New Book</h1> } />
 
             </div>
         );
