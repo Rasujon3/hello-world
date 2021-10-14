a0e92176ff6954245df0a22a0336d6a10ac1beca
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-10-14 11:52:15 +0600

    Controlled Component

--- src/components/representational/NewBook.js
+++ src/components/representational/NewBook.js
@@ -1,12 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const NewBook = (props) => {
-    console.log(props);
-    return (
-        <div>
-            <h1>New Book Entry</h1>
-        </div>
-    );
-};
+class NewBook extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            bookName: "",
+            writer: "",
+            description: "",
+        }
+    }
+
+    handleInputChange = event => {
+        const name = event.target.name;
+        const value = event.target.value;
+        this.setState({
+            [name]: value
+        })
+    }
+
+    componentDidUpdate() {
+        console.log(this.state);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>New Book Entry</h1>
+                <form>
+
+                    <label>Book Name: </label>
+                    <br />
+                    <input onChange={(event) => this.handleInputChange(event)}
+                        value={this.state.bookName} name="bookName" type="text" />
+                    <br />
+                    <label>Writer: </label>
+                    <br />
+                    <input onChange={(event) => this.handleInputChange(event)}
+                        value={this.state.writer} name="writer" type="text" />
+                    <br />
+                    <label>Description: </label>
+                    <br />
+                    <textarea onChange={(event) => this.handleInputChange(event)}
+                        value={this.state.description} name="description" />
+                    <br />
+                    <input value="submit" type="submit" />
+
+                </form>
+            </div>
+        );
+    }
+}
 
 export default NewBook;
